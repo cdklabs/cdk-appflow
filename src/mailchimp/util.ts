3097b4b6a8e7c121f9fe37324bf66b9f265575f4
Remi <remitache@gmail.com>
2024-03-19 09:09:46 +0100

    :zap: MailChimp Connector

---
+++ src/mailchimp/util.ts
@@ -0,0 +1,11 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+/**
+ * An enum representing the MailChimp API versions.
+ */
+export enum MailChimpApiVersion {
+    V3 = '3.0'
+  }
