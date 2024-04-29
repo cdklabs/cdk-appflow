4e49ef44552f69ab7a0972d274bcbf13232ecbad
Remi <remitache@gmail.com>
2024-04-29 21:53:30 +0200

    :rocket: google ads connector

---
+++ src/googleads/source.ts
@@ -0,0 +1,55 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+import { IFlow } from '../core/flows';
+import { ConnectorType } from '../core/connectors/connector-type';
+import { ISource } from '../core/vertices';
+import { IConstruct } from 'constructs';
+import { CfnFlow } from 'aws-cdk-lib/aws-appflow';
+import { GoogleAdsConnectorProfile } from './profile';
+import { GoogleAdsConnectorType } from './type';
+/**
+ * Properties of a Google Ads v4 Source
+ */
+export interface GoogleAdsSourceProps {
+    readonly profile: GoogleAdsConnectorProfile;
+    readonly apiVersion: string;
+    readonly object: string;
+}
+/**
+ * A class that represents a Google Ads v4 Source
+ */
+export class GoogleAdsSource implements ISource {
+    /**
+     * The AppFlow type of the connector that this source is implemented for
+     */
+    public readonly connectorType: ConnectorType = GoogleAdsConnectorType.instance;
+
+    constructor(private readonly props: GoogleAdsSourceProps) { }
+
+    bind(scope: IFlow): CfnFlow.SourceFlowConfigProperty {
+
+        this.tryAddNodeDependency(scope, this.props.profile);
+    
+        return {
+            connectorType: this.connectorType.asProfileConnectorType,
+            connectorProfileName: this.props.profile.name,
+            apiVersion: this.props.apiVersion,
+            sourceConnectorProperties: this.buildSourceConnectorProperties(),
+        };
+    }
+
+    private buildSourceConnectorProperties(): CfnFlow.SourceConnectorPropertiesProperty {
+        return {
+            customConnector: {
+                entityName: this.props.object,
+            },
+        };
+    }
+    private tryAddNodeDependency(scope: IConstruct, resource?: IConstruct | string): void {
+        if (resource && typeof resource !== 'string') {
+            scope.node.addDependency(resource);
+        }
+    }
+}
---
+++ src/googleads/type.ts
@@ -0,0 +1,26 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+import { ConnectorType } from '../core/connectors/connector-type';
+
+/**
+ * @internal
+ */
+export class GoogleAdsConnectorType extends ConnectorType {
+    /**
+     * Singleton
+     */
+    public static get instance(): ConnectorType {
+        if (!GoogleAdsConnectorType.actualInstance) {
+            GoogleAdsConnectorType.actualInstance = new GoogleAdsConnectorType();
+        }
+        return GoogleAdsConnectorType.actualInstance;
+    }
+    
+    private static actualInstance: ConnectorType;
+    
+    constructor() {
+        super('GoogleAds', true);
+    }
+}
