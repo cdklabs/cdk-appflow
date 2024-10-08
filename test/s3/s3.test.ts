0d5dbaccc9b5e8d695fd49ece0d76049f47ee33d
Rafał Pawłaszek <rpawlasz@amazon.com>
2024-10-08 11:59:31 +0200

    feat: exposing role as a prop parameter & limiting the auto-generated role's permissions

--- test/s3/s3.test.ts
+++ test/s3/s3.test.ts
@@ -2,8 +2,10 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
+import { Database } from '@aws-cdk/aws-glue-alpha';
 import { Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import { Role } from 'aws-cdk-lib/aws-iam';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import {
   Mapping,
@@ -90,7 +92,7 @@ describe('S3 Flow tests', () => {
     });
   });
 
-  test('Source and Destination with a bucket name', () => {
+  test('Destination with complex formatting ', () => {
     const stack = new Stack(undefined, undefined);
 
     const bucket = Bucket.fromBucketName(stack, 'TestBucket', 'test-bucket');
@@ -143,9 +145,7 @@ describe('S3 Flow tests', () => {
                 },
                 FileType: 'CSV',
                 PrefixConfig: {
-                  PathPrefixHierarchy: [
-                    'SCHEMA_VERSION',
-                  ],
+                  PathPrefixHierarchy: ['SCHEMA_VERSION'],
                   PrefixFormat: 'HOUR',
                 },
               },
@@ -172,7 +172,201 @@ describe('S3 Flow tests', () => {
           TaskProperties: [
             {
               Key: 'EXCLUDE_SOURCE_FIELDS_LIST',
-              Value: '[\"field1\",\"field2\"]',
+              Value: '["field1","field2"]',
+            },
+          ],
+          TaskType: 'Map_all',
+        },
+      ],
+      TriggerConfig: {
+        TriggerType: 'OnDemand',
+      },
+    });
+  });
+
+  test('Destination with catalog', () => {
+    const stack = new Stack(undefined, undefined);
+
+    const bucket = Bucket.fromBucketName(stack, 'TestBucket', 'test-bucket');
+
+    const source = new S3Source({
+      bucket: bucket,
+      prefix: 'source-prefix',
+      format: {
+        type: S3InputFileType.JSON,
+      },
+    });
+
+    const destination = new S3Destination({
+      location: { bucket: bucket },
+      catalog: {
+        database: new Database(stack, 'TestDb', {
+          databaseName: 'testdb',
+        }),
+        tablePrefix: 'tablePrefix',
+      },
+    });
+
+    new OnDemandFlow(stack, 'testFlow', {
+      source: source,
+      destination,
+      mappings: [Mapping.mapAll({ exclude: ['field1', 'field2'] })],
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::S3::Bucket', 0);
+    template.resourceCountIs('AWS::AppFlow::Flow', 1);
+    template.resourceCountIs('AWS::IAM::Role', 1);
+    template.resourceCountIs('AWS::Glue::Database', 1);
+
+    template.hasResourceProperties('AWS::AppFlow::Flow', {
+      DestinationFlowConfigList: [
+        {
+          ConnectorType: 'S3',
+          DestinationConnectorProperties: {
+            S3: {
+              BucketName: 'test-bucket',
+            },
+          },
+        },
+      ],
+      MetadataCatalogConfig: {
+        GlueDataCatalog: {
+          DatabaseName: {
+            Ref: 'TestDb6EF03243',
+          },
+          RoleArn: {
+            'Fn::GetAtt': ['testFlowGlueAccessRole34DC638A', 'Arn'],
+          },
+          TablePrefix: 'tablePrefix',
+        },
+      },
+      FlowName: 'testFlow',
+      SourceFlowConfig: {
+        ConnectorType: 'S3',
+        SourceConnectorProperties: {
+          S3: {
+            BucketName: 'test-bucket',
+            BucketPrefix: 'source-prefix',
+          },
+        },
+      },
+      Tasks: [
+        {
+          ConnectorOperator: {
+            S3: 'NO_OP',
+          },
+          SourceFields: [],
+          TaskProperties: [
+            {
+              Key: 'EXCLUDE_SOURCE_FIELDS_LIST',
+              Value: '["field1","field2"]',
+            },
+          ],
+          TaskType: 'Map_all',
+        },
+      ],
+      TriggerConfig: {
+        TriggerType: 'OnDemand',
+      },
+    });
+  });
+
+  test('Destination with catalog with custom role and imported values', () => {
+    const stack = new Stack(undefined, undefined);
+
+    const bucket = Bucket.fromBucketName(stack, 'TestBucket', 'test-bucket');
+    const database = Database.fromDatabaseArn(
+      stack,
+      'TestDb',
+      stack.formatArn({
+        service: 'glue',
+        resource: 'database',
+        resourceName: 'test-db',
+      }),
+    );
+    const role = Role.fromRoleName(stack, 'TestRole', 'test-role');
+
+    const source = new S3Source({
+      bucket: bucket,
+      prefix: 'source-prefix',
+      format: {
+        type: S3InputFileType.JSON,
+      },
+    });
+
+    const destination = new S3Destination({
+      location: { bucket: bucket },
+      catalog: {
+        role,
+        database,
+        tablePrefix: 'tablePrefix',
+      },
+    });
+
+    new OnDemandFlow(stack, 'testFlow', {
+      source: source,
+      destination,
+      mappings: [Mapping.mapAll({ exclude: ['field1', 'field2'] })],
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::S3::Bucket', 0);
+    template.resourceCountIs('AWS::AppFlow::Flow', 1);
+    template.resourceCountIs('AWS::IAM::Role', 0);
+    template.resourceCountIs('AWS::Glue::Database', 0);
+
+    template.hasResourceProperties('AWS::AppFlow::Flow', {
+      DestinationFlowConfigList: [
+        {
+          ConnectorType: 'S3',
+          DestinationConnectorProperties: {
+            S3: {
+              BucketName: 'test-bucket',
+            },
+          },
+        },
+      ],
+      MetadataCatalogConfig: {
+        GlueDataCatalog: {
+          DatabaseName: 'test-db',
+          RoleArn: {
+            'Fn::Join': [
+              '',
+              [
+                'arn:',
+                { Ref: 'AWS::Partition' },
+                ':iam::',
+                { Ref: 'AWS::AccountId' },
+                ':role/test-role',
+              ],
+            ],
+          },
+          TablePrefix: 'tablePrefix',
+        },
+      },
+      FlowName: 'testFlow',
+      SourceFlowConfig: {
+        ConnectorType: 'S3',
+        SourceConnectorProperties: {
+          S3: {
+            BucketName: 'test-bucket',
+            BucketPrefix: 'source-prefix',
+          },
+        },
+      },
+      Tasks: [
+        {
+          ConnectorOperator: {
+            S3: 'NO_OP',
+          },
+          SourceFields: [],
+          TaskProperties: [
+            {
+              Key: 'EXCLUDE_SOURCE_FIELDS_LIST',
+              Value: '["field1","field2"]',
             },
           ],
           TaskType: 'Map_all',
